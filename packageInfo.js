5bf457bd3254e79459d54cc88c3bc375af87d0a0
SirSquirrel09 <168587164+SirSquirrel09@users.noreply.github.com>
2024-08-25 00:56:37 +0200

    Updated to 1.1.0

--- packageInfo.js
+++ packageInfo.js
@@ -2,13 +2,26 @@ const fs = require("fs")
 const path = require("path")
 
 function getName() {
+    if(!fs.existsSync(path.join(__dirname, "./package.json"))) {
+        return "Not Found"
+    }
     return JSON.parse(fs.readFileSync(path.join(__dirname, "./package.json"))).packageName
 }
 
+function getDev() {
+    if(!fs.existsSync(path.join(__dirname, "./package.json"))) {
+        return "Not Found"
+    }
+    return JSON.parse(fs.readFileSync(path.join(__dirname, "./package.json"))).devmode
+}
+
 function getVersion() {
+    if(!fs.existsSync(path.join(__dirname, "./package.json"))) {
+        return "Not Found"
+    }
     return JSON.parse(fs.readFileSync(path.join(__dirname, "./package.json"))).version
 }
 
-module.exports = {getName, getVersion}
+module.exports = {getName, getVersion, getDev}
 
 //Useless cuz i just discovered app.getVersion() but i don't feel like changing it now.
